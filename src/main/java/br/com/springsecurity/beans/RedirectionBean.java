3d6e925f0080787a59821924efd47f60a4fc01aa
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-13 15:11:13 -0200

    Criada classe de redirecionamento

---
+++ src/main/java/br/com/springsecurity/beans/RedirectionBean.java
@@ -0,0 +1,24 @@
+package br.com.springsecurity.beans;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Scope("request")
+public class RedirectionBean {
+	public String userPage() {
+		return "userPage";
+	}
+
+	public String adminPage() {
+		return "adminPage";
+	}
+
+	public String index() {
+		return "index";
+	}
+
+	public String sucessfullpage() {
+		return "sucessfullpage";
+	}
+}
