59584f88063898f9fe015d335ee3e6e02485c6f7
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-13 12:38:11 -0200

    Criado a implementacao da classe login service

---
+++ src/main/java/br/com/springsecurity/service/impl/LoginServiceImpl.java
@@ -0,0 +1,35 @@
+package br.com.springsecurity.service.impl;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.springsecurity.dao.UserDAO;
+import br.com.springsecurity.entities.User;
+import br.com.springsecurity.service.LoginService;
+
+@Service
+@Transactional
+public class LoginServiceImpl implements LoginService {
+
+	@Autowired
+	private UserDAO dao;
+
+	public User login(String username, String password) throws IllegalArgumentException {
+		if (isEmptyOrNull(username) || isEmptyOrNull(password)) {
+			throw new IllegalArgumentException("Atenção, username ou password vazios!");
+		}
+
+		User u = dao.login(username, password);
+
+		if (u == null) {
+			throw new IllegalArgumentException("Erro: username ou passowrd incorretos!");
+		}
+		return u;
+	}
+
+	private boolean isEmptyOrNull(String s) {
+		return s == null || s.equals("");
+	}
+}
