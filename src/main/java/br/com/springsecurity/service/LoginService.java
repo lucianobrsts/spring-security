fa64e5c7fd144a80030e9783350d7bb220b4f68a
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-13 12:39:49 -0200

    Criado classe loginService

--- src/main/java/br/com/springsecurity/service/LoginService.java
+++ src/main/java/br/com/springsecurity/service/LoginService.java
@@ -1,5 +1,9 @@
 package br.com.springsecurity.service;
 
+import br.com.springsecurity.entities.User;
+
 public interface LoginService {
+	
+	User login(String username, String password) throws IllegalArgumentException;
 
 }
