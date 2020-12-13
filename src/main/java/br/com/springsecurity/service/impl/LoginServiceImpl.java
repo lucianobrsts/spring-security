09ea54b173b215e3f5b2410e79dbea5a9729f0cb
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-13 15:09:18 -0200

    Criada classe loginServiceImpl

--- src/main/java/br/com/springsecurity/service/impl/LoginServiceImpl.java
+++ src/main/java/br/com/springsecurity/service/impl/LoginServiceImpl.java
@@ -1,9 +1,8 @@
 package br.com.springsecurity.service.impl;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.springsecurity.dao.UserDAO;
 import br.com.springsecurity.entities.User;
