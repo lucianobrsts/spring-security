f293958030fbb7004ec1ccf911d239dbcb0cf1b4
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-13 12:03:22 -0200

    Finalizados metodos da classe userDAO

--- src/main/java/br/com/springsecurity/dao/UserDAO.java
+++ src/main/java/br/com/springsecurity/dao/UserDAO.java
@@ -1,5 +1,21 @@
 package br.com.springsecurity.dao;
 
+import java.util.List;
+
+import br.com.springsecurity.entities.User;
+
 public interface UserDAO {
+
+	List<User> findAll();
+
+	void save(User user);
+
+	void update(User user);
+
+	void remove(User user);
+
+	void getById(Long id);
+
+	User login(String username, String password);
 
 }
